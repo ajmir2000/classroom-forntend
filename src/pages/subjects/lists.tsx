5e80d0363d5e59c2958fe5fde29c7f0188958877
Mohammad Ajmir <ajmerfaqiri14.af@gmail.com>
2026-02-26 21:19:42 +0430

    update app.css and list component

--- src/pages/subjects/lists.tsx
+++ src/pages/subjects/lists.tsx
@@ -1,5 +1,12 @@
+import { Breadcrumb } from "@/components/refine-ui/layout/breadcrumb";
+import { ListView } from "@/components/refine-ui/views/list-view";
 import React from "react";
 
 export default function SubjectsList() {
-  return <div>List</div>;
+  return (
+    <ListView>
+      <Breadcrumb />
+      <h1 className="page-title">Subjects</h1>
+    </ListView>
+  );
 }
